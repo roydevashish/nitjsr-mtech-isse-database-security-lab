26645a6011982daa1ddf2b06dd730261a2deca22
Devashish Roy <medevashishroy@gmail.com>
2025-01-22 12:07:42 +0530

    Fixed Assignment 01
    Fixed the datatype for client_master table.

--- Assignment-01.sql
+++ Assignment-01.sql
@@ -1,12 +1,12 @@
 -- Create the following tables:
 -- Table: client_master
 CREATE TABLE client_master (
-    client_no varchar(10)(6),
-    name varchar(10) (20),
-    address1 varchar(10)(30),
-    address2 varchar(10)(30),
-    city varchar(10)(15),
-    state varchar(10)(15),
+    client_no varchar(6),
+    name varchar(20),
+    address1 varchar(30),
+    address2 varchar(30),
+    city varchar(15),
+    state varchar(15),
     pincode numeric(6),
     bal_due DECIMAL(10,2)
 )
@@ -43,7 +43,8 @@ VALUES
 ('P07865', '1.22 floppies', 5, 'piece', 100, 20, 525, 500),
 ('P07868', 'Keyboards', 2, 'piece', 10, 3, 3150, 3050),
 ('P07885', 'CD Drive', 2.5, 'piece', 10, 3, 5250, 5100),
-('P07965', '540 HDD', 4, 'piece', 10, 3, 8400, 8000),
+('P
+07965', '540 HDD', 4, 'piece', 10, 3, 8400, 8000),
 ('P07975', '1.44 Drive', 5, 'piece', 10, 3, 1050, 1000),
 ('P08865', '1.22 Drive', 5, 'piece', 2, 3, 1050, 1000)
 
